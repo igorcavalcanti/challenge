c888e2a85f7da62f2cb48e0e9404f90dbf8f0f25
Igor Ramos <igor.ramos.external@bt.com>
2021-12-13 07:37:13 -0300

    Adicionando um cache a verificação de existência de verificação anterior.

--- src/main/java/com/example/melichallenge/MelichallengeApplication.java
+++ src/main/java/com/example/melichallenge/MelichallengeApplication.java
@@ -2,8 +2,10 @@ package com.example.melichallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MelichallengeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/melichallenge/repository/DNACheckRepository.java
+++ src/main/java/com/example/melichallenge/repository/DNACheckRepository.java
@@ -1,10 +1,12 @@
 package com.example.melichallenge.repository;
 
 import com.example.melichallenge.model.DNACheck;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface DNACheckRepository extends JpaRepository<DNACheck, Long> {
+    @Cacheable(value="DNACheck", unless="#result == null")
     Optional<DNACheck> findByHashEquals(String hash);
 }
