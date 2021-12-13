b39d7461d2f5b1b2938e4472cb5c26dbf6054149
Igor Ramos <igor.ramos.external@bt.com>
2021-12-12 21:02:05 -0300

    Ajuste no script de inicialização

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,3 @@
-create table dna_check (id bigint not null, hash varchar(255), result boolean, primary key (id));
-create table dna_check_stats (id bigint not null, humans_count decimal(19,2), simians_count decimal(19,2), primary key (id));
-create sequence hibernate_sequence start with 1 increment by 1;
+create table if not exists dna_check (id bigint not null, hash varchar(255), result boolean, primary key (id));
+create table if not exists dna_check_stats (id bigint not null, humans_count decimal(19,2), simians_count decimal(19,2), primary key (id));
+create sequence if not exists hibernate_sequence start with 1 increment by 1;
